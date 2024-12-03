948004e3ca3af80ff1b68344376cabdbe72a66b6
Yaroslav Markivskyi <yaroslav.markivskyi.it.2022@lpnu.ua>
2024-12-03 08:58:39 +0200

    feat: add Flasgger for API documentation, update Docker configuration, and enhance article routes with Swagger integration

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from .modules.articles import articles_bp
 from .commands.create_user import create_user
 from .commands.init_database import init_db_command
 
+from flasgger import Swagger
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(articles_bp)
@@ -11,6 +12,23 @@ app.register_blueprint(articles_bp)
 
 app.cli.add_command(create_user)
 app.cli.add_command(init_db_command)
+
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec_1",
+            "route": "/apispec_1.json",
+            "rule_filter": lambda rule: True,  # Include all endpoints
+            "model_filter": lambda tag: True,  # Include all models
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/swagger/",
+}
+swagger = Swagger(app, config=swagger_config)
 
 
 if __name__ == '__main__':
--- app/modules/articles/routers.py
+++ app/modules/articles/routers.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from http import HTTPStatus, HTTPMethod
 from flask import Blueprint
 from flask_jwt_extended import jwt_required
+from flasgger import swag_from
 
 from .crud_services import ArticleService
 from .serializers import ArticleSerializer
@@ -14,6 +15,7 @@ articles_bp = Blueprint('articles', __name__, url_prefix='/articles')
 
 @articles_bp.route('/', methods=[HTTPMethod.GET])
 @jwt_required()
+@swag_from("./swagger.yml")
 def get_articles():
     entity_articles = ArticleService.get_all_articles()
     json_articles = [ArticleSerializer.to_dict(article) for article in entity_articles]
@@ -22,6 +24,7 @@ def get_articles():
 
 @articles_bp.route("/<int:article_id>", methods=[HTTPMethod.GET])
 @jwt_required()
+@swag_from("./swagger.yml")
 def get_article(article_id):
     entity_article = ArticleService.get_article_by_id(article_id)
     ArticleValidator.is_exist_article(entity_article)
@@ -31,6 +34,7 @@ def get_article(article_id):
 
 @articles_bp.route("/", methods=[HTTPMethod.POST])
 @jwt_required()
+@swag_from("./swagger.yml")
 def add_article():
     data = request.json
     ArticleValidator.is_exist_fields(data)
@@ -43,6 +47,7 @@ def add_article():
 @articles_bp.route("/<int:article_id>", methods=[HTTPMethod.PUT])
 @jwt_required()
 @editor_required
+@swag_from("./swagger.yml")
 def update_article(article_id):
     data = request.json
     ArticleValidator.is_exist_fields(data)
@@ -59,6 +64,7 @@ def update_article(article_id):
 @articles_bp.route("/<int:article_id>", methods=[HTTPMethod.DELETE])
 @jwt_required()
 @admin_required
+@swag_from("./swagger.yml")
 def delete_article(article_id):
     ArticleService.delete_article(article_id)
     return jsonify({"message": "Book deleted successfully"}), HTTPStatus.OK
